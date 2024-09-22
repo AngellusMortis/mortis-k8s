647aa672fd001209841bb41f70a919a381076c78
Christopher Bailey <cbailey@mort.is>
2024-09-22 17:32:31 -0400

    Update zsh config

--- iac/nix/common/all.nix
+++ iac/nix/common/all.nix
@@ -92,7 +92,28 @@
         zsh
     ];
 
-    programs.zsh.enable = true;
+    programs.zsh = {
+        enable = true;
+        enableCompletion = true;
+        autosuggestion.enable = true;
+        syntaxHighlighting.enable = true;
+
+        shellAliases = {
+            ll = "ls -l";
+            update = "sudo nixos-rebuild switch";
+        };
+        history = {
+            size = 10000;
+            path = "${config.xdg.dataHome}/zsh/history";
+        };
+        zplug = {
+            enable = true;
+            plugins = [
+                { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+                { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+            ];
+        };
+    };
 
     # Some programs need SUID wrappers, can be configured further or are
     # started in user sessions.
