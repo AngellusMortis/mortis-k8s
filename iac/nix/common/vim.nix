555df64c0c1bb14504661815a1827de4db6d21cb
Christopher Bailey <cbailey@mort.is>
2024-09-22 23:51:15 -0400

    Add python3 support for vim

--- iac/nix/common/vim.nix
+++ iac/nix/common/vim.nix
@@ -4,7 +4,7 @@
 
     environment.systemPackages = with pkgs; [
         (
-            (vim_configurable.override { python = python3; }).customize {
+            (vim_configurable.override { python3 = python3; }).customize {
                 name = "vim";
                 vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
                     start = [ vim-nix ]; # load plugins on startup
