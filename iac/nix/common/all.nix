8ec5415772d7c9de1c8e3f29d685cd696803db42
Christopher Bailey <cbailey@mort.is>
2024-09-22 21:27:24 -0400

    More configuration for zsh

--- iac/nix/common/all.nix
+++ iac/nix/common/all.nix
@@ -85,7 +85,14 @@
 
         programs.zsh = {
             enable = true;
-            interactiveShellInit = ''
+            enableBashCompletion = true;
+            enableCompletion = true;
+            enableFzfCompletion = true;
+            enableFzfGit = true;
+            enableFzfHistory = true;
+            enableLsColors = true;
+            enableSyntaxHighlighting = true;
+            promptInit = ''
                 # load system specific local configs
                 if [ -r ~/.local/zshrc ]; then
                     source ~/.local/zshrc
@@ -118,7 +125,14 @@
 
         programs.zsh = {
             enable = true;
-            interactiveShellInit = ''
+            enableBashCompletion = true;
+            enableCompletion = true;
+            enableFzfCompletion = true;
+            enableFzfGit = true;
+            enableFzfHistory = true;
+            enableLsColors = true;
+            enableSyntaxHighlighting = true;
+            promptInit = ''
                 # Disable tmux for VS Code
                 if [ -n "$\{VSCODE_AGENT_FOLDER+1}" ]; then
                     export USE_TMUX=false
@@ -188,7 +202,6 @@
 
         shellInit = ''
             # envs
-            export LS_COLORS='rs=0:di=01;36:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:'
             export LESS='-R --use-color -Dd+r$Du+b'
             export MANPAGER="less -R --use-color -Dd+r -Du+b"
             export LC_ALL=en_US.UTF-8
@@ -216,7 +229,7 @@
         '';
         interactiveShellInit = ''
             bindkey -e
-            bindkey '^R' history-incremental-search-backward
+            # bindkey '^R' history-incremental-search-backward
             bindkey "\e[3~" delete-char
             bindkey "^[[1;5C" forward-word
             bindkey "^[[1;5D" backward-word
