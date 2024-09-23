3507714529fa5b61fa04842b585b92afda17ddc5
Christopher Bailey <cbailey@mort.is>
2024-09-22 22:19:48 -0400

    Tweak tmux-session

--- iac/nix/common/tmux-session.nix
+++ iac/nix/common/tmux-session.nix
@@ -1,25 +1,22 @@
-{
-  my-file = writeTextFile {
-    name = "tmux-session";
-    text = ''
-        #!/usr/bin/env bash
+{ pkgs, ... }:
 
-        trim() { echo $1; }
+let tmuxSession = pkgs.writeShellScriptBin "tmux-session" ''
+    trim() { echo $1; }
 
-        # Make sure we are not already in a tmux session
-        if [ -z "$TMUX" ]; then
-            base_session=`whoami`
-            tmux_nb=$(trim `tmux ls | grep "^$base_session" | wc -l`)
-            if [ "$tmux_nb" = "0" ]; then
-                echo "Launching tmux base session $base_session ..."
-                tmux -2 new-session -s $base_session
-            else
-                # Attach to the new session & kill it once orphaned
-                tmux -2 attach-session -t $base_session
-            fi
+    # Make sure we are not already in a tmux session
+    if [ -z "$TMUX" ]; then
+        base_session=`whoami`
+        tmux_nb=$(trim `tmux ls | grep "^$base_session" | wc -l`)
+        if [ "$tmux_nb" = "0" ]; then
+            echo "Launching tmux base session $base_session ..."
+            tmux -2 new-session -s $base_session
+        else
+            # Attach to the new session & kill it once orphaned
+            tmux -2 attach-session -t $base_session
         fi
-    '';
-    executable = true;
-    destination = "/usr/bin/tmux-session";
-  };
+    fi
+'';
+
+in {
+  environment.systemPackages = [ tmuxSession ];
 }
