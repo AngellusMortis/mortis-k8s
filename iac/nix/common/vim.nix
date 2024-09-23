6791549055132bb7f4a4d32e3d372e2df2c4955e
Christopher Bailey <cbailey@mort.is>
2024-09-22 23:49:11 -0400

    Add python3 support for vim

--- iac/nix/common/vim.nix
+++ iac/nix/common/vim.nix
@@ -1,10 +1,15 @@
 { pkgs, ... }:
 {
-  programs.vim.defaultEditor = true;
+    programs.vim.defaultEditor = true;
 
-  environment.systemPackages = with pkgs; [
-    ((vim_configurable.override { python = python3; }).customize {
-      name = "vim";
-    })
-  ];
+    environment.systemPackages = with pkgs; [
+        (
+            (vim_configurable.override { python = python3; }).customize {
+                name = "vim";
+                vimrcConfig.packages.myplugins = with pkgs.vimPlugins; {
+                    start = [ vim-nix ]; # load plugins on startup
+                };
+            }
+        )
+    ];
 }
