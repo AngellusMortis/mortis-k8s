fb38a53a0733f1092ee03726a9e2ce7cd71ea8de
Christopher Bailey <cbailey@mort.is>
2024-09-29 09:38:27 -0400

    Tweak grub

--- iac/nix/common/grub-theme.nix
+++ iac/nix/common/grub-theme.nix
@@ -5,25 +5,22 @@
 { config, lib, pkgs, ... }:
 
 {
+    nixpkgs.config.packageOverrides = pkgs: rec { mortis-plymouth = pkgs.callPackage ./mortis-plymouth.nix {}; };
+
     boot = {
         plymouth = {
             enable = true;
-            theme = "rings";
-            themePackages = with pkgs; [
-                # By default we would install all themes
-                (adi1090x-plymouth-themes.override {
-                    selected_themes = [ "rings" ];
-                })
-            ];
+            theme = "mortis";
+            themePackages = [ pkgs.mortis-plymouth ];
         };
 
         # Enable "Silent Boot"
         consoleLogLevel = 0;
         initrd.verbose = false;
+        initrd.systemd.enable = true;
         kernelParams = [
             "quiet"
             "splash"
-            "boot.shell_on_fail"
             "loglevel=3"
             "rd.systemd.show_status=false"
             "rd.udev.log_level=3"
@@ -33,5 +30,13 @@
         # It's still possible to open the bootloader list by pressing any key
         # It will just not appear on screen unless a key is pressed
         loader.timeout = 0;
+    };
+
+    systemd.watchdog.rebootTime = "0";
+    i18n.defaultLocale = "en_US.UTF-8";
+    console = {
+        font = "ter-v32n";
+        keyMap = "us";
+        packages = with pkgs; [ terminus_font ];
     };
 }
