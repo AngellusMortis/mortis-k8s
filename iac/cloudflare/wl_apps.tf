16dd83d439035194ccc41c1674f20e3546cc0630
Christopher Bailey <cbailey@mort.is>
2024-10-03 13:32:08 -0400

    Add auth record

---
+++ iac/cloudflare/wl_apps.tf
@@ -0,0 +1,40 @@
+resource "cloudflare_record" "wl_auth" {
+    zone_id = cloudflare_zone.mortis.id
+    name = "auth.wl"
+    proxied = true
+    content = "${cloudflare_zero_trust_tunnel_cloudflared.wl.id}.cfargotunnel.com"
+    type = "CNAME"
+    tags = concat(local.tags.wl, local.tags.control)
+}
+
+# resource "cloudflare_zero_trust_access_policy" "allow_admin_users" {
+#     account_id = local.account_id
+#     name = "Allow Admin Users"
+#     decision = "allow"
+
+#     include {
+#         group = [cloudflare_zero_trust_access_group.admin_users.id]
+#     }
+
+#     require {
+#         group = [cloudflare_zero_trust_access_group.admin_users.id]
+#     }
+# }
+
+# resource "cloudflare_zero_trust_access_application" "dc_syncthing" {
+#     account_id = local.account_id
+#     name = "SyncThing (Backup)"
+#     domain = "sync.dc.${cloudflare_zone.mortis.zone}"
+#     type = "self_hosted"
+#     session_duration = "24h"
+
+#     allow_authenticate_via_warp = false
+#     app_launcher_visible = false
+#     logo_url = "https://syncthing.net/img/favicons/apple-touch-icon-152x152.png"
+#     auto_redirect_to_identity = true
+#     allowed_idps = [cloudflare_zero_trust_access_identity_provider.authentik.id]
+#     http_only_cookie_attribute = true
+#     policies = [
+#         cloudflare_zero_trust_access_policy.allow_admin_users.id
+#     ]
+# }
