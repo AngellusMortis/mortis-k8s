1aac8a603c417fe1cbdf7d8a93c72e813453fc44
Christopher Bailey <cbailey@mort.is>
2024-10-03 13:59:24 -0400

    Add back tunnel

--- iac/cloudflare/tunnels.tf
+++ iac/cloudflare/tunnels.tf
@@ -6,12 +6,12 @@ resource "random_bytes" "wl_tunnel_secret" {
     length = 128
 }
 
-# resource "cloudflare_zero_trust_tunnel_cloudflared" "wl" {
-#     account_id = local.account_id
-#     name = "wl.mort.is"
-#     secret = random_bytes.wl_tunnel_secret.base64
-#     config_src = "local"
-# }
+resource "cloudflare_zero_trust_tunnel_cloudflared" "wl" {
+    account_id = local.account_id
+    name = "wl.mort.is"
+    secret = random_bytes.wl_tunnel_secret.base64
+    config_src = "local"
+}
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "dc" {
     account_id = local.account_id
