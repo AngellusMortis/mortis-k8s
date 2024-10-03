68428b81ba4b1e507df2614fbed275cc3f1e3713
Christopher Bailey <cbailey@mort.is>
2024-10-03 13:17:30 -0400

    Add wl.mort.is tunnel

--- iac/cloudflare/outputs.tf
+++ iac/cloudflare/outputs.tf
@@ -8,6 +8,16 @@ output "dc_tunnel_secret" {
     sensitive = true
 }
 
+output "wl_tunnel_id" {
+    value = cloudflare_zero_trust_tunnel_cloudflared.dc.id
+    sensitive = false
+}
+
+output "wl_tunnel_secret" {
+    value = cloudflare_zero_trust_tunnel_cloudflared.dc.secret
+    sensitive = true
+}
+
 output "uptime_robot_token_id" {
     value = cloudflare_zero_trust_access_service_token.uptime_robot.client_id
     sensitive = false
--- iac/cloudflare/tunnels.tf
+++ iac/cloudflare/tunnels.tf
@@ -1,10 +1,21 @@
-resource "random_bytes" "tunnel_secret" {
-  length = 128
+resource "random_bytes" "dc_tunnel_secret" {
+    length = 128
+}
+
+resource "random_bytes" "wl_tunnel_secret" {
+    length = 128
+}
+
+resource "cloudflare_zero_trust_tunnel_cloudflared" "wl" {
+    account_id = local.account_id
+    name = "wl.mort.is"
+    secret = random_bytes.wl_tunnel_secret.base64
+    config_src = "local"
 }
 
 resource "cloudflare_zero_trust_tunnel_cloudflared" "dc" {
     account_id = local.account_id
     name = "dc.mort.is"
-    secret = random_bytes.tunnel_secret.base64
+    secret = random_bytes.dc_tunnel_secret.base64
     config_src = "local"
 }
