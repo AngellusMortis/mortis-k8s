77e493d0019962da46c346f672da56d24550d01d
Christopher Bailey <cbailey@mort.is>
2024-10-04 16:03:23 -0400

    Fix deluge theme

--- iac/nix/common/mortis-deluge.nix
+++ iac/nix/common/mortis-deluge.nix
@@ -3,12 +3,17 @@
 with pkgs;
 
 let
-  delugeTheme = ../../deluge;
-  mortisDeluge = pkgs.symlinkJoin {
-    name = "mortis-deluge";
-    paths = [ pkgs.deluge delugeTheme ];
-    version = "2.1.1-1";
-  };
+    delugeTheme = ../../deluge;
+    mortisDeluge = pkgs.symlinkJoin {
+        name = "mortis-deluge";
+        paths = [ pkgs.deluge delugeTheme ];
+        version = "2.1.1-1";
+        postBuild = ''
+            srcBin=$(readlink $out/bin/deluge-web)
+            rm $out/bin/deluge-web
+            cp $srcBin $out/bin/deluge-web
+        '';
+    };
 in {
-  services.deluge.package = mortisDeluge;
+    services.deluge.package = mortisDeluge;
 }
