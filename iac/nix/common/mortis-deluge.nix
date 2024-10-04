a98065cae2dae353239f06f454f6deaa8f2286ab
Christopher Bailey <cbailey@mort.is>
2024-10-04 16:16:41 -0400

    Fix deluge theme

--- iac/nix/common/mortis-deluge.nix
+++ iac/nix/common/mortis-deluge.nix
@@ -10,11 +10,7 @@ let
         version = "2.1.1-1";
         postBuild = ''
             themesDir=$out/lib/python3.12/site-packages/deluge/ui/web/themes/css
-            oldCss=$(readlink $themesDir/xtheme-dark.css)
-            oldMobileCss=$(readlink $themesDir/xtheme-dark-mobile.css)
-            rm $themesDir/xtheme-dark.css $themesDir/xtheme-dark-mobile.css
-            cp $oldCss $themesDir/xtheme-dark.css
-            cp $oldMobileCss $themesDir/xtheme-dark-mobile.css
+            cp $themesDir/xtheme-dark.css $themesDir/xtheme-gray.css
         '';
     };
 in {
