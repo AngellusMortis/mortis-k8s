e4843d9a956ff25febdc4f2f62939c74911b0dbc
Christopher Bailey <cbailey@mort.is>
2025-06-22 16:47:20 -0400

    Remove old iac

--- iac/dc/backup/nix/backup/default.nix
+++
@@ -1,6 +0,0 @@
-let
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/heads/nixos-unstable.tar.gz";
-in
-import (nixpkgs + "/nixos/lib/eval-config.nix") {
-  modules = [ ./configuration.nix ];
-}
