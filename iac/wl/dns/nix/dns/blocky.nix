c2387c32f644aaf390fc8edcb9417c64b5ad534f
Christopher Bailey <cbailey@mort.is>
2025-07-25 15:58:47 -0400

    Update DNS

--- iac/wl/dns/nix/dns/blocky.nix
+++ iac/wl/dns/nix/dns/blocky.nix
@@ -255,8 +255,8 @@
                 };
                 zone = ''
                     $ORIGIN wl.mort.is.
-                    _minecraft._tcp.mc.wl.mort.is.  IN  SRV 0 5 25565 mc-srv.wl.mort.is.
-                    _minecraft._tcp.mc.             IN  SRV 0 5 25565 mc-srv.wl.mort.is.
+                    _minecraft._tcp.mc.wl.mort.is.  IN  SRV 0 5 25565 mc-server.wl.mort.is.
+                    _minecraft._tcp.mc.             IN  SRV 0 5 25565 mc-server.wl.mort.is.
                 '';
             };
 
