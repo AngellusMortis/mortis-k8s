b6b5279c0f528d7777609ebd3938800256ee5776
Christopher Bailey <cbailey@mort.is>
2026-05-24 11:19:21 -0400

    fix authentik tf

--- iac/authentik/proxy_app/main.tf
+++ iac/authentik/proxy_app/main.tf
@@ -6,6 +6,11 @@ data "authentik_flow" "default_provider_authorization_implicit_consent" {
     slug = "default-provider-authorization-implicit-consent"
 }
 
+data "authentik_flow" "default_provider_invalidation_flow" {
+    slug = "default-provider-invalidation-flow"
+}
+
+
 
 resource "authentik_provider_proxy" "provider" {
     count = var.create_provider ? 1 : 0
